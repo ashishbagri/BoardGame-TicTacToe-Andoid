36753eaba0e59097580087b4d7607b6dc18b4023
ashish.bagri@ymail.com <ashish.bagri@ymail.com>
2017-01-19 23:48:38 +0530

    simple computer play

---
+++ app/src/main/java/in/codengles/tictactoe/BestMove.java
@@ -0,0 +1,7 @@
+package in.codengles.tictactoe;
+
+/**
+ * Created by PaRV on 1/19/2017.
+ */
+public class BestMove {
+}
---
+++ app/src/main/java/in/codengles/tictactoe/ComputerPlay.java
@@ -0,0 +1,7 @@
+package in.codengles.tictactoe;
+
+/**
+ * Created by PaRV on 1/18/2017.
+ */
+public class ComputerPlay {
+}
